c1c0af42dc2b07b4ec8d37f0feb4e39461ed90af
Ezequiel <ezecarrascosa@hotmail.com>
2020-09-03 18:22:03 -0300

    adding backend objects

---
+++ Backend/e-pedago/src/main/kotlin/com/example/epedago/EPedagoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.epedago
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EPedagoApplication
+
+fun main(args: Array<String>) {
+	runApplication<EPedagoApplication>(*args)
+}
---
+++ Backend/e-pedago/src/test/kotlin/com/example/epedago/EPedagoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.epedago
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EPedagoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
